43e790c8541baf7933163745ebaf7923e910978a
Ruslan <4irus86@gmail.com>
2025-08-23 12:04:59 +0300

    fix: calendar

--- dev/js/calendar.js
+++ dev/js/calendar.js
@@ -25,6 +25,28 @@ const calendar = new FullCalendar.Calendar(calendarEl, {
         matchUrl: '#'
       }
     },
+     {
+      start: '2025-08-10',
+      extendedProps: {
+        homeLogo: '/images/4ead8b11dd6fecdfb3ae789f2a4ab8af.png',
+        awayLogo: '/images/5f9e9d3862d184bc6a36f384c1dd3ee9.png',
+        homeShort: 'tor',
+        awayShort: 'bos',
+        time: '2 : 0',
+        matchUrl: '#'
+      }
+    },
+    {
+      start: '2025-08-16',
+      extendedProps: {
+        homeLogo: '/images/4ead8b11dd6fecdfb3ae789f2a4ab8af.png',
+        awayLogo: '/images/5f9e9d3862d184bc6a36f384c1dd3ee9.png',
+        homeShort: 'tor',
+        awayShort: 'bos',
+        time: '2 : 0',
+        matchUrl: '#'
+      }
+    },
   ],
   eventContent: function (arg) {
     const { homeLogo, awayLogo, homeShort, awayShort, time, matchUrl } = arg.event.extendedProps;
